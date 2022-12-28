3a828ea9c9de2eca230eb24852dc810255d0e3da
larry <26318510+larry0x@users.noreply.github.com>
2022-12-28 19:08:16 +0000

    bump cw-ownable version

--- contracts/hub/src/lib.rs
+++ contracts/hub/src/lib.rs
@@ -1,7 +1,7 @@
 use cosmwasm_std::{
     entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult,
 };
-use cw_ownable::{Ownership, OwnershipError, OWNERSHIP};
+use cw_ownable::OwnershipError;
 
 use steak::hub::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
@@ -16,7 +16,7 @@ pub fn instantiate(
     msg: InstantiateMsg,
 ) -> StdResult<Response> {
     cw2::set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-    cw_ownable::initialize_owner(deps, &msg.owner)?;
+    cw_ownable::initialize_owner(deps.storage, deps.api, &msg.owner)?;
     Ok(Response::default())
 }
 
@@ -30,7 +30,7 @@ pub fn execute(
     match msg {
         ExecuteMsg::UpdateOwnership(action) => {
             cw_ownable::update_ownership(deps, &env.block, &info.sender, action)?;
-        }
+        },
     }
     Ok(Response::default())
 }
@@ -39,12 +39,8 @@ pub fn execute(
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::Ownership {} => {
-            let ownership = OWNERSHIP.load(deps.storage)?;
-            to_binary(&Ownership {
-                owner: ownership.owner.map(String::from),
-                pending_owner: ownership.pending_owner.map(String::from),
-                pending_expiry: ownership.pending_expiry,
-            })
+            let ownership = cw_ownable::get_ownership(deps.storage)?;
+            to_binary(&ownership)
         },
     }
 }
